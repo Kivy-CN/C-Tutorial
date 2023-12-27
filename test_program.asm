ea2bf4918625142b59633bb49aaf4a170b3739a7
Frederick <softcheck@outlook.com>
2023-12-27 17:25:24 +0800

    Update test_program.asm

--- test_program.asm
+++ test_program.asm
@@ -1,46 +1,24 @@
 section .data
-    a db 10
-    b db 20
-    result db 0
-    output db "The result is: ", 0
-    buffer db 0
+    result db "Result: %d", 0
 
 section .text
     global _start
 
 _start:
-    ; 将变量a的值存入寄存器AL
-    mov al, byte [a]
-    
-    ; 将变量b的值存入寄存器BL
-    mov bl, byte [b]
-    
-    ; 将寄存器AL和BL相加，并将结果存入寄存器AL
-    add al, bl
-    
-    ; 将寄存器AL的值存入变量result
-    mov byte [result], al
-    
-    ; 将结果转换为字符形式并存储在缓冲区中
-    mov ah, 0
-    add al, '0'
-    mov byte [buffer], al
-    
-    ; 打印输出结果前的字符串
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, output
-    mov edx, 15
-    int 0x80
-    
-    ; 打印输出结果
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, buffer
-    mov edx, 2
-    int 0x80
-    
+    ; 计算10 + 20
+    mov eax, 10
+    add eax, 20
+
+    ; 将结果打印到终端命令行
+    push eax
+    push result
+    call printf
+    add esp, 8
+
     ; 退出程序
     mov eax, 1
     xor ebx, ebx
     int 0x80
+
+section .data
+    extern printf
