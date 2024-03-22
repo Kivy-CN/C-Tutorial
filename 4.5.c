161d735aafc403509342e696022e01e5b4da7a9f
Frederick <softcheck@outlook.com>
2024-03-22 18:05:20 +0800

    update

--- 4.5.c
+++ 4.5.c
@@ -1,23 +1,20 @@
-#include <stdio.h>
-
+#include <stdio.h> 
 int main()
-
 {
+    // 定义变量a，类型为int，赋值为10
+    int a = 10;
+    // 定义变量b，类型为float，赋值为3.14
+    float b = 3.14;
+    // 定义变量c，类型为double，将a和b相加
+    double c = a + b; // 隐式将int类型转换为double类型
 
-   // 定义变量a，类型为int，赋值为10
-
-   int a;
-
-   a = 3;
-
-   float b = 3.14;
-
-   int c = (int)b; // 显式将float转成int
-
-   // 打印变量a和b的值
-
-   printf("c = %f \t b = %f\n\n", c, b);
-
-   return 0;
-
+    // 定义变量d，类型为double，赋值为3.14159
+    double d = 3.14159;
+    // 将d转换为int类型，赋值给e
+    int e = (int)d; // 显式将double类型转换为int类型
+    // 打印变量c和e的值
+    printf("e = %f \tc = %d\n", e, c);
+    printf("e = %d \tc = %d\n", e, c);
+    printf("e = %f \tc = %d\n", e, c);
+    return 0;
 }
